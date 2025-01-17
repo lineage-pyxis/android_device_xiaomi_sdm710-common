6c7d492b66c161867f8cbac101f0257fd9716ef3
Ivan Vecera <ivan@cera.cz>
2025-01-17 19:27:02 +0100

    sdm710-common: Build and enable fastcharge HAL
    * Set fastcharge path to /sys/class/qcom-battery/restricted_charging
    * Build fastcharge HAL
    * Allow fastcharge HAL to access the sysfs file
    
    Change-Id: Idbd8f301f5cf80616c76fa2b6fd0c7b9afbaa840
    Signed-off-by: Ivan Vecera <ivan@cera.cz>

--- sdm710.mk
+++ sdm710.mk
@@ -162,6 +162,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.default
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
